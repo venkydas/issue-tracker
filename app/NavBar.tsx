d8197454459c128d3546de1241acc5673f8a994b
Dasari-Venkatesh <venkateshdasari021@gmail.com>
2024-11-21 10:21:38 +0530

    Initialized NavBar

---
+++ app/NavBar.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Link from 'next/link'
+import { DiAndroid } from "react-icons/di";
+
+
+const NavBar = () => {
+    const links = [
+        { label : 'Dashboard', href: '/'},
+        { label : 'Issues', href: '/issues'},
+    ]
+
+  return (
+    <nav className='flex '>
+      
+    </nav>
+  )
+}
+
+export default NavBar
